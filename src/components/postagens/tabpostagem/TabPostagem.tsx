390b438f5aac88ae16b4597cfa434316259c0f67
GabiSilvaBelo <gabybelosp@gmail.com>
2023-05-03 15:13:55 -0300

    Corrigindo Tab Postagem

--- src/components/postagens/tabpostagem/TabPostagem.tsx
+++ src/components/postagens/tabpostagem/TabPostagem.tsx
@@ -2,20 +2,22 @@ import { useState } from 'react'
 import { Box } from "@mui/material"
 import { AppBar, Tab, Tabs, Typography } from '@material-ui/core'
 import { TabContext, TabPanel } from '@material-ui/lab'
-import ListaPostagem from '../listapostagem/ListaPostagem'
+
+import ListaPostagem from '../listapostagem/ListaPostagem';
+import './TabPostagem.css';
 
 
 function TabPostagem() {
-    const [value, setValue] = useState('1')
-    function handleChange(event: React.ChangeEvent<{}>, newValue: string){
-        setValue(newValue);
-    }
+  const [value, setValue] = useState('1')
+  function handleChange(event: React.ChangeEvent<{}>, newValue: string) {
+    setValue(newValue);
+  }
   return (
     <>
       <TabContext value={value}>
         <AppBar position="static">
           <Tabs centered indicatorColor="secondary" onChange={handleChange}>
-            <Tab label="Todas as postagens" value="1"/>
+            <Tab label="Todas as postagens" value="1" />
             <Tab label="Sobre-nós" value="2" />
           </Tabs>
         </AppBar>
